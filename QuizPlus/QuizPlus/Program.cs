f62c42b73059edb7283ae1254796dff3f7ae9580
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-07-21 18:59:54 +0900

    더하기문제

---
+++ QuizPlus/QuizPlus/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace QuizPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tens, units, sum;
+            int result = 0;
+            int cnt = 0;
+            int n;
+            string saveNum = "";
+
+
+            do
+            {
+                Console.Write("입력(1~99) : ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("입력된 값 : " + n);
+                if (n <= 0 || n > 99) Console.WriteLine("범위 내의 수를 입력하세요.");
+            } while (n <= 0 || n > 99);
+
+
+            if (n < 10)
+            {
+                sum = n * 11;
+                units = sum / 10;
+                saveNum += sum;
+            }
+            else
+            {
+                sum = n;
+                tens = sum / 10;
+                units = sum % 10;
+                sum = tens + units;
+                
+                result = (units * 10) + sum;
+                saveNum += result;
+            }
+            
+
+            
+            while (result != n){
+                cnt++;
+                tens = units;
+                units = sum % 10;
+                sum = tens + units;
+                if (cnt != 1)
+                {
+                    result = (tens * 10) + units;
+                    saveNum += " " + result;
+                }
+            }
+            Console.WriteLine(saveNum);
+            Console.WriteLine("출력 : " + cnt);
+            
+        }
+        
+    }
+
+    
+}
