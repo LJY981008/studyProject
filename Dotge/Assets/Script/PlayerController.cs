09154dd172161392554bff6a8a25c9432e525a27
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-07-22 18:30:08 +0900

    플레이어 이동, 게임오버 함수 작성

---
+++ Dotge/Assets/Script/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Rigidbody 프로퍼티, 속도 값
+    public Rigidbody playerRigidbody;
+    public float speed = 0.8f;
+    
+    void Update()
+    {
+        //키보드 입력 이벤트
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+
+    }
+    //게임오버 이벤트
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
