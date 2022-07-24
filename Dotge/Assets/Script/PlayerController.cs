3172be9190a3fbbef21f764c04278ca0caed486e
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-07-24 18:05:05 +0900

    조작 방식 변경

--- Dotge/Assets/Script/PlayerController.cs
+++ Dotge/Assets/Script/PlayerController.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Rigidbody 프로퍼티, 속도 값
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
     
+
+    
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
         //키보드 입력 이벤트 화살표 조종
-        if (Input.GetKey(KeyCode.UpArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
 
     }
     //게임오버 이벤트
