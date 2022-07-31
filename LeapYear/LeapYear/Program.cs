aad3c32f20cc8bd85196d1b3496580760df96a0f
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-07-31 20:33:45 +0900

    윤년

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("입력 : ");
+            string str = Console.ReadLine();
+            int year = Convert.ToInt32(str);
+            int result = 0;
+
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) result = 1;
+
+            Console.WriteLine("결과 : " + result);
+        }
+    }
+}
