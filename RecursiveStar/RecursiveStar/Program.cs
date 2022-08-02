ef19b4d5946793d6de88f7029885dc26c897ed75
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2022-08-02 22:52:38 +0900

    3*3 별찍기 문제

---
+++ RecursiveStar/RecursiveStar/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace RecursiveStar
+{
+    class Program
+    {
+       
+        static void Bound(int row, int col, int n)
+        {
+
+            if (n == 3)
+            {
+                if (row % n == 1 && col % n == 1) Console.Write(" ");
+                else Console.Write("*");
+            }
+            else if ((row / (n / 3)) % 3 == 1 && (col / (n / 3)) % 3 == 1) Console.Write(" ");
+            else Bound(row, col, n / 3);
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("1~7의 수 입력 : ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (0 < k && k < 8)
+            {
+                int num = (int)Math.Pow(3, k);
+                Console.WriteLine("값 : " + num);
+                for(int i = 0; i < num; i++)
+                {
+                    for(int j = 0; j < num; j++)
+                    {
+                        Bound(i, j, num);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("범위안의 수를 입력해 주세요.");
+            }
+
+        }
+        
+        
+    }
+}
